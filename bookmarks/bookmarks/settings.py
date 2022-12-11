1243ae2be9bf2ebb981828ae1cdc44c0e0bc7a24
Jayachitra <jayachitra@testpress.in>
2022-12-11 20:46:31 +0530

    Refactor:setting

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = "bookmarks.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "social_media",
-        "USER": "postgres",
-        "PASSWORD": "1234",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
