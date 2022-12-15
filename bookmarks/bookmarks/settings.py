a984851d0ba81a9ee46bf7e476ce2e576c5cfc63
Jayachitra <jayachitra@testpress.in>
2022-12-15 12:34:00 +0530

    Add social authentication to the site

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
+    "social_django",
 ]
 
 MIDDLEWARE = [
